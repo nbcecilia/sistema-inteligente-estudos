f132db49f2eda50ce68ea0bbe673091154a8da1f
Cecília Nunes <nbricecilia@gmail.com>
2026-05-26 10:30:40 -0300

    Revert "iniciadoteste"

--- cadastro.php
+++ cadastro.php
@@ -3,11 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro - Cecilia</title>
-    <style>
-
-        
-    </style>
+    <title>Cadastro</title>
 </head>
 <body>
     
